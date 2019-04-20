882a8bfed129d560ccfd8d183dacb15eb4ace73f
Mike Hawkins <43323751+hawk5963@users.noreply.github.com>
2019-04-20 18:42:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -44,6 +44,7 @@
 					},
 					changeLoc: function () {
 						var request = {
+							//this url needs to be updated, it's currently just going to the same place as the input box for latlng
 							url: "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + inputbox.latitude + "&lon=" + inputbox.longitude,
 							dataType: "json",
 							success: locationData
@@ -51,8 +52,11 @@
 						$.ajax(request);
 						function locationData (data)
 						{
-							this.latitude = data[0].lat;
-							this.longitude = data[0].lon;
+							console.log(data.display_name.substr(0, data.display_name.indexOf(',')));
+							this.latitude = data.lat;
+							this.longitude = data.lon;
+							console.log(this.latitude);
+							console.log(this.longitude);
 							mymap.panTo(new L.LatLng(this.latitude, this.longitude));
 						}
 						this.latlng = false;
@@ -318,7 +322,7 @@
 
 			//this function is passed the fetched data from openaq and then loops through the measurment locations examining their lat & lon coordinates
 			function locations2(data){
-	
+
 				var locationLat2;
 				var locationLon2;
 
@@ -360,7 +364,7 @@
 					else if(value >=.207){
 						return hazardous;
 					}
-				}//o3			
+				}//o3
 				if(param == "pm25"){
 					if(value <= 12.0){
 						return good;
